07b8cfb9f02f66e229e410b527c05bfa23449d4e
Alex1um <nothingorno@gmail.com>
2022-10-15 02:48:14 +0700

    using object ids now

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::env::*;
 
 const AREA_MAX_X: usize = 17;
 const AREA_MAX_Y: usize = 8;
-const AREA_MAX_Z: usize = 2;
+const AREA_MAX_Z: usize = 3;
 
 #[cfg(target_arch = "wasm32")]
 extern "C" {
@@ -24,24 +24,22 @@ fn main() {
   let mut area = Area::new(AREA_MAX_X, AREA_MAX_Y, AREA_MAX_Z).create_ref();
   let mut players: Vec<GameObjectRef> = Vec::new();
   let mut nishals: Vec<GameObjectRef> = Vec::new();
-  let ply = GameObject::new(2, Position::new(AREA_MAX_X / 2, AREA_MAX_Y / 2, 1)).create_ref();
-  ply.borrow_mut().set_area(&area);
+  let ply = GameObject::new(2, Position::new(AREA_MAX_X / 2, AREA_MAX_Y / 2, 2));
   {
     let mut area = area.borrow_mut();
     for x in 0..AREA_MAX_X {
       for y in 0..AREA_MAX_Y {
-        let obj = GameObject::new(0, Position::new(x, y, 0)).create_ref();
+        let obj = GameObject::new(0, Position::new(x, y, 0));
         area.insert(obj).expect("Successful adding");
         if (y == 0 || y == AREA_MAX_Y - 1) && x % 2 == 0 || (x == 0 || x == AREA_MAX_X - 1) && y % 2 == 0 {
-          let nish = GameObject::new(1, Position::new(x, y, 1)).create_ref();
-          nishals.push(Rc::clone(&nish));
-          area.insert(nish).expect("Successful adding");
+          let nish = GameObject::new(1, Position::new(x, y, 1));
+          nishals.push(area.insert(nish).expect("Successful adding"));
         }
       }
     }
-    players.push(Rc::clone(&ply));
-    area.insert(Rc::clone(&ply));
   }
+  let ply = area.borrow_mut().insert(ply).expect("Successful adding player");
+  players.push(ply);
   let view = View::new(&area, Position::new(0, 0, 0), AREA_MAX_X, AREA_MAX_Y, AREA_MAX_Z);
   let screen = Screen::new(view, resolution.0 / AREA_MAX_X, resolution.1 / AREA_MAX_Y);
   let ( creator, mut render) = Render::new(screen, resolution.0, resolution.1).expect("render created");
@@ -59,8 +57,7 @@ fn main() {
     render.render();
     let mut new_pos = Position::new(0, 0, 0);
     {
-      let mut player = ply.borrow_mut();
-      let Position {x: cx , y: cy, z: cz} = player.get_pos();
+      let Position {x: cx , y: cy, z: cz} = area.borrow().get_object_pos(ply).expect("play still in objects array");
       new_pos.set(cx, cy, cz);
       // println!("before: {} {}", cx, cy);
       if cx == AREA_MAX_X - 1 || cx == 0 {
@@ -72,7 +69,8 @@ fn main() {
       }
       new_pos.y = ((new_pos.y as isize) + dy) as usize;
       // println!("after: {} {}", new_pos.x, new_pos.y);
-      player.set_pos(new_pos).expect("Successful setpos");
+      area.borrow_mut().update_object(ply, new_pos);
+      // player.set_pos(new_pos).expect("Successful setpos");
     }
     #[cfg(not(target_arch = "wasm32"))]
     sleep(Duration::from_millis(200));
